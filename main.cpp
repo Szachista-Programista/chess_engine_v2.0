94c9bf948ad6a5acf6d4b6b389e343fa4bfefa08
Szachista-Programista <krzysztofkazanecki6@gmail.com>
2024-05-09 16:16:39 +0200

    add computing bitboard functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <bitset>
 #include <fstream>
+//#include <thread>////////////
+//#include <mutex>//////////
+//#include <future>///////////////
+#include <chrono>////////////
+
+
+using namespace std;///////////////
+using namespace std::chrono;/////////////////
+//std::mutex whiteMutex;///////////////
+//std::mutex blackMutex;/////////////////
 
 enum BitBoardIndex
 {
@@ -19,27 +29,97 @@ enum BitBoardIndex
     blackKing,
 
     whitePiece,
+    whiteCapturedSquare,
     blackPiece,
-    anyPiece
+    blackCapturedSquare,
+    anyPiece,
+    emptySquare,
+    bitBoardSize
 };
+enum Movements
+{
+    up = 8,
+    upRight = 7,
+    rightt = 1,
+    downRight = 9,
+    down = 8,
+    downLeft = 7,
+    leftt = 1,
+    upLeft = 9,
+
+    upUp = 16,
+    downDown = 16,
+
+    upUpRight = 15,
+    upRightRight = 6,
+    downRightRight = 10,
+    downDownRight = 17,
+    downDownLeft = 15,
+    downLeftLeft = 6,
+    upLeftLeft = 10,
+    upUpLeft = 17
+};
+
 
 struct errorType{std::string errorMessage;};
 using BitBoardPtr = std::bitset<64>*;
 using ChessBoardPtr = char(*)[8];
+const std::bitset<64> SINGLE_RIGHT_EDGE_MASK = 0xfefefefefefefefe;
+const std::bitset<64> DOUBLE_RIGHT_EDGE_MASK = 0xfcfcfcfcfcfcfcfc;
+const std::bitset<64> SINGLE_LEFT_EDGE_MASK  = 0x7f7f7f7f7f7f7f7f;
+const std::bitset<64> DOUBLE_LEFT_EDGE_MASK  = 0x3f3f3f3f3f3f3f3f;
 
 BitBoardPtr initializeBitBoardPtr();
     BitBoardPtr loadBitBoard();
         std::string readChessBoardFile();
         void rewriteFileContentToBitBoard(std::string fileContent, BitBoardPtr ptr);
-    void computeBitBoard(BitBoardPtr ptr);
+void computeBitBoard(BitBoardPtr ptr);
+    void computeSquareCapturedByWhite(BitBoardPtr ptr);
+        void computeSquareCapturedByWhitePawn(BitBoardPtr ptr);
+        void computeSquareCapturedByWhiteKnight(BitBoardPtr ptr);
+        void computeSquareCapturedByWhiteBishop(BitBoardPtr ptr);
+        void computeSquareCapturedByWhiteBishopRook(BitBoardPtr ptr);
+        void computeSquareCapturedByWhiteQueen(BitBoardPtr ptr);
+        void computeSquareCapturedByWhiteKing(BitBoardPtr ptr);
+    void computeSquareCapturedByBlack(BitBoardPtr ptr);
+        void computeSquareCapturedByBlackPawn(BitBoardPtr ptr);
+        void computeSquareCapturedByBlackKnight(BitBoardPtr ptr);
+        void computeSquareCapturedByBlackBishop(BitBoardPtr ptr);
+        void computeSquareCapturedByBlackBishopRook(BitBoardPtr ptr);
+        void computeSquareCapturedByBlackQueen(BitBoardPtr ptr);
+        void computeSquareCapturedByBlackKing(BitBoardPtr ptr);
 void writeChessboard(BitBoardPtr BBPtr);
     ChessBoardPtr rewriteBitBoardTo8x8CharArray(BitBoardPtr BBPtr);
+void writeBitSet(const std::bitset<64> &bs);
+bool isWhiteKingChecked(BitBoardPtr ptr);
+bool isBlackKingChecked(BitBoardPtr ptr);
+
 
 int main()
 {
     BitBoardPtr x = initializeBitBoardPtr();
-    computeBitBoard(x);
     writeChessboard(x);
+
+   /*
+    unsigned long long iterations = 0;    auto start = std::chrono::steady_clock::now();    const int time_limit_seconds = 2;
+    while (true)
+    {
+        computeBitBoard(x);
+
+    iterations++;    auto current = std::chrono::steady_clock::now(); if (std::chrono::duration_cast<std::chrono::seconds>(current - start).count() >= time_limit_seconds) break;
+    }
+    std::cout << "Program wykonano przez " << time_limit_seconds << " sekundy." << std::endl << "Liczba wykonanych iteracji: " << iterations << std::endl;
+    */
+
+ /* for(int i=0; i<bitBoardSize; i++)
+    {
+        std::cout<<"//////////////////////////////////////////////"<<std::endl<<">>>   "<<i<<"   <<<"<<std::endl;
+        writeBitSet(x[i]);
+
+    }*/
+
+
+
     return 0;
 }
 
@@ -54,7 +134,7 @@ BitBoardPtr initializeBitBoardPtr()
 {
     std::string fileContent = readChessBoardFile();
     BitBoardPtr bitBoard;
-    try {bitBoard = new std::bitset<64>[20]{};}
+    try {bitBoard = new std::bitset<64>[bitBoardSize]{};}
     catch(const std::bad_alloc &e)
     {
         errorType x;
@@ -62,6 +142,7 @@ BitBoardPtr initializeBitBoardPtr()
         throw x;
     }
     rewriteFileContentToBitBoard(fileContent, bitBoard);
+    computeBitBoard(bitBoard);
     return bitBoard;
 }
         std::string readChessBoardFile()
@@ -95,7 +176,7 @@ BitBoardPtr initializeBitBoardPtr()
     try
     {
         for (int i = 0; i < 64; i++)
-            switch (fileContent[i])
+            switch (fileContent[63-i])
             {
                 case '*': break;
                 case 'P': ptr[whitePawn  ].set(i); break;
@@ -120,11 +201,243 @@ BitBoardPtr initializeBitBoardPtr()
         throw x;
     }
 }
-    void computeBitBoard(BitBoardPtr ptr)
+void computeBitBoard(BitBoardPtr ptr)
 {
-    ptr[whitePiece] = ptr[whitePawn]  | ptr[whiteKnight] | ptr[whiteBishop] | ptr[whiteRook] | ptr[whiteQueen] | ptr[whiteKing];
-    ptr[blackPiece] = ptr[blackPawn]  | ptr[blackKnight] | ptr[blackBishop] | ptr[blackRook] | ptr[blackQueen] | ptr[blackKing];
-    ptr[anyPiece]   = ptr[whitePiece] | ptr[blackPiece];
+    ptr[whitePiece]  = ptr[whitePawn]  | ptr[whiteKnight] | ptr[whiteBishop] | ptr[whiteRook] | ptr[whiteQueen] | ptr[whiteKing];
+    ptr[blackPiece]  = ptr[blackPawn]  | ptr[blackKnight] | ptr[blackBishop] | ptr[blackRook] | ptr[blackQueen] | ptr[blackKing];
+    ptr[anyPiece]    = ptr[whitePiece] | ptr[blackPiece];
+    ptr[emptySquare] = ~ptr[anyPiece];
+
+    computeSquareCapturedByWhite(ptr);
+    computeSquareCapturedByBlack(ptr);
+}
+    void computeSquareCapturedByWhite(BitBoardPtr ptr)
+{
+    computeSquareCapturedByWhitePawn(ptr);
+    computeSquareCapturedByWhiteKnight(ptr);
+    computeSquareCapturedByWhiteBishop(ptr);
+    computeSquareCapturedByWhiteBishopRook(ptr);
+    computeSquareCapturedByWhiteQueen(ptr);
+    computeSquareCapturedByWhiteKing(ptr);
+}
+        void computeSquareCapturedByWhitePawn(BitBoardPtr ptr)
+{
+    ptr[whiteCapturedSquare] |= (ptr[whitePawn] << upLeft)  & SINGLE_RIGHT_EDGE_MASK;
+    ptr[whiteCapturedSquare] |= (ptr[whitePawn] << upRight) & SINGLE_LEFT_EDGE_MASK;
+}
+        void computeSquareCapturedByWhiteKnight(BitBoardPtr ptr)
+{
+    ptr[whiteCapturedSquare] |= (ptr[whiteKnight] << upUpRight)      & SINGLE_LEFT_EDGE_MASK;
+    ptr[whiteCapturedSquare] |= (ptr[whiteKnight] << upRightRight)   & DOUBLE_LEFT_EDGE_MASK;
+    ptr[whiteCapturedSquare] |= (ptr[whiteKnight] >> downRightRight) & DOUBLE_LEFT_EDGE_MASK;
+    ptr[whiteCapturedSquare] |= (ptr[whiteKnight] >> downDownRight)  & SINGLE_LEFT_EDGE_MASK;
+    ptr[whiteCapturedSquare] |= (ptr[whiteKnight] >> downDownLeft)   & SINGLE_RIGHT_EDGE_MASK;
+    ptr[whiteCapturedSquare] |= (ptr[whiteKnight] >> downLeftLeft)   & DOUBLE_RIGHT_EDGE_MASK;
+    ptr[whiteCapturedSquare] |= (ptr[whiteKnight] << upLeftLeft)     & DOUBLE_RIGHT_EDGE_MASK;
+    ptr[whiteCapturedSquare] |= (ptr[whiteKnight] << upUpLeft)       & SINGLE_RIGHT_EDGE_MASK;
+}
+        void computeSquareCapturedByWhiteBishop(BitBoardPtr ptr)
+{
+    std::bitset<64> urMove = ptr[whiteBishop]; //u-up/r-right/d-down/l-left
+    std::bitset<64> drMove = ptr[whiteBishop];
+    std::bitset<64> dlMove = ptr[whiteBishop];
+    std::bitset<64> ulMove = ptr[whiteBishop];
+
+    while((urMove | drMove | dlMove | ulMove).any())
+    {
+        urMove = (urMove << upRight)   & SINGLE_LEFT_EDGE_MASK;
+        drMove = (drMove >> downRight) & SINGLE_LEFT_EDGE_MASK;
+        dlMove = (dlMove >> downLeft)  & SINGLE_RIGHT_EDGE_MASK;
+        ulMove = (ulMove << upLeft)    & SINGLE_RIGHT_EDGE_MASK;
+
+        ptr[whiteCapturedSquare] |= urMove | drMove | dlMove | ulMove;
+
+        urMove &= ptr[emptySquare];
+        drMove &= ptr[emptySquare];
+        dlMove &= ptr[emptySquare];
+        ulMove &= ptr[emptySquare];
+    }
+}
+        void computeSquareCapturedByWhiteBishopRook(BitBoardPtr ptr)
+{
+    std::bitset<64> uMove = ptr[whiteRook]; //u-up/r-right/d-down/l-left
+    std::bitset<64> rMove = ptr[whiteRook];
+    std::bitset<64> dMove = ptr[whiteRook];
+    std::bitset<64> lMove = ptr[whiteRook];
+
+    while((uMove | rMove | dMove | lMove).any())
+    {
+        uMove = (uMove << up);
+        rMove = (rMove >> rightt) & SINGLE_LEFT_EDGE_MASK;
+        dMove = (dMove >> down);
+        lMove = (lMove << leftt)  & SINGLE_RIGHT_EDGE_MASK;
+
+        ptr[whiteCapturedSquare] |= uMove | rMove | dMove | lMove;
+
+        uMove &= ptr[emptySquare];
+        rMove &= ptr[emptySquare];
+        dMove &= ptr[emptySquare];
+        lMove &= ptr[emptySquare];
+    }
+}
+        void computeSquareCapturedByWhiteQueen(BitBoardPtr ptr)
+{
+    std::bitset<64> uMove  = ptr[whiteQueen]; //u-up/r-right/d-down/l-left
+    std::bitset<64> urMove = ptr[whiteQueen];
+    std::bitset<64> rMove  = ptr[whiteQueen];
+    std::bitset<64> drMove = ptr[whiteQueen];
+    std::bitset<64> dMove  = ptr[whiteQueen];
+    std::bitset<64> dlMove = ptr[whiteQueen];
+    std::bitset<64> lMove  = ptr[whiteQueen];
+    std::bitset<64> ulMove = ptr[whiteQueen];
+
+    while((uMove | urMove | rMove | drMove | dMove | dlMove | lMove | ulMove).any())
+    {
+        uMove  = (uMove  << up);
+        urMove = (urMove << upRight)   & SINGLE_LEFT_EDGE_MASK;
+        rMove  = (rMove  >> rightt)    & SINGLE_LEFT_EDGE_MASK;
+        drMove = (drMove >> downRight) & SINGLE_LEFT_EDGE_MASK;
+        dMove  = (dMove  >> down);
+        dlMove = (dlMove >> downLeft)  & SINGLE_RIGHT_EDGE_MASK;
+        lMove  = (lMove  << leftt)     & SINGLE_RIGHT_EDGE_MASK;
+        ulMove = (ulMove << upLeft)    & SINGLE_RIGHT_EDGE_MASK;
+
+        ptr[whiteCapturedSquare] |= uMove | urMove | rMove | drMove | dMove | dlMove | lMove | ulMove;
+
+        uMove  &= ptr[emptySquare];
+        urMove &= ptr[emptySquare];
+        rMove  &= ptr[emptySquare];
+        drMove &= ptr[emptySquare];
+        dMove  &= ptr[emptySquare];
+        dlMove &= ptr[emptySquare];
+        lMove  &= ptr[emptySquare];
+        ulMove &= ptr[emptySquare];
+    }
+}
+        void computeSquareCapturedByWhiteKing(BitBoardPtr ptr)
+{
+    ptr[whiteCapturedSquare] |= (ptr[whiteKing] << up);
+    ptr[whiteCapturedSquare] |= (ptr[whiteKing] << upRight)   & SINGLE_LEFT_EDGE_MASK;
+    ptr[whiteCapturedSquare] |= (ptr[whiteKing] >> rightt)    & SINGLE_LEFT_EDGE_MASK;
+    ptr[whiteCapturedSquare] |= (ptr[whiteKing] >> downRight) & SINGLE_LEFT_EDGE_MASK;
+    ptr[whiteCapturedSquare] |= (ptr[whiteKing] >> down);
+    ptr[whiteCapturedSquare] |= (ptr[whiteKing] >> downLeft)  & SINGLE_RIGHT_EDGE_MASK;
+    ptr[whiteCapturedSquare] |= (ptr[whiteKing] << leftt)     & SINGLE_RIGHT_EDGE_MASK;
+    ptr[whiteCapturedSquare] |= (ptr[whiteKing] << upLeft)    & SINGLE_RIGHT_EDGE_MASK;
+}
+    void computeSquareCapturedByBlack(BitBoardPtr ptr)
+{
+    computeSquareCapturedByBlackPawn(ptr);
+    computeSquareCapturedByBlackKnight(ptr);
+    computeSquareCapturedByBlackBishop(ptr);
+    computeSquareCapturedByBlackBishopRook(ptr);
+    computeSquareCapturedByBlackQueen(ptr);
+    computeSquareCapturedByBlackKing(ptr);
+}
+        void computeSquareCapturedByBlackPawn(BitBoardPtr ptr)
+{
+    ptr[blackCapturedSquare] |= (ptr[blackPawn] >> downRight) & SINGLE_LEFT_EDGE_MASK;
+    ptr[blackCapturedSquare] |= (ptr[blackPawn] >> downLeft) & SINGLE_RIGHT_EDGE_MASK;
+}
+        void computeSquareCapturedByBlackKnight(BitBoardPtr ptr)
+{
+    ptr[blackCapturedSquare] |= (ptr[blackKnight] << upUpRight)      & SINGLE_LEFT_EDGE_MASK;
+    ptr[blackCapturedSquare] |= (ptr[blackKnight] << upRightRight)   & DOUBLE_LEFT_EDGE_MASK;
+    ptr[blackCapturedSquare] |= (ptr[blackKnight] >> downRightRight) & DOUBLE_LEFT_EDGE_MASK;
+    ptr[blackCapturedSquare] |= (ptr[blackKnight] >> downDownRight)  & SINGLE_LEFT_EDGE_MASK;
+    ptr[blackCapturedSquare] |= (ptr[blackKnight] >> downDownLeft)   & SINGLE_RIGHT_EDGE_MASK;
+    ptr[blackCapturedSquare] |= (ptr[blackKnight] >> downLeftLeft)   & DOUBLE_RIGHT_EDGE_MASK;
+    ptr[blackCapturedSquare] |= (ptr[blackKnight] << upLeftLeft)     & DOUBLE_RIGHT_EDGE_MASK;
+    ptr[blackCapturedSquare] |= (ptr[blackKnight] << upUpLeft)       & SINGLE_RIGHT_EDGE_MASK;
+}
+        void computeSquareCapturedByBlackBishop(BitBoardPtr ptr)
+{
+    std::bitset<64> urMove = ptr[blackBishop]; //u-up/r-right/d-down/l-left
+    std::bitset<64> drMove = ptr[blackBishop];
+    std::bitset<64> dlMove = ptr[blackBishop];
+    std::bitset<64> ulMove = ptr[blackBishop];
+
+    while((urMove | drMove | dlMove | ulMove).any())
+    {
+        urMove = (urMove << upRight)   & SINGLE_LEFT_EDGE_MASK;
+        drMove = (drMove >> downRight) & SINGLE_LEFT_EDGE_MASK;
+        dlMove = (dlMove >> downLeft)  & SINGLE_RIGHT_EDGE_MASK;
+        ulMove = (ulMove << upLeft)    & SINGLE_RIGHT_EDGE_MASK;
+
+        ptr[blackCapturedSquare] |= urMove | drMove | dlMove | ulMove;
+
+        urMove &= ptr[emptySquare];
+        drMove &= ptr[emptySquare];
+        dlMove &= ptr[emptySquare];
+        ulMove &= ptr[emptySquare];
+    }
+}
+        void computeSquareCapturedByBlackBishopRook(BitBoardPtr ptr)
+{
+    std::bitset<64> uMove = ptr[blackRook]; //u-up/r-right/d-down/l-left
+    std::bitset<64> rMove = ptr[blackRook];
+    std::bitset<64> dMove = ptr[blackRook];
+    std::bitset<64> lMove = ptr[blackRook];
+
+    while((uMove | rMove | dMove | lMove).any())
+    {
+        uMove = (uMove << up);
+        rMove = (rMove >> rightt) & SINGLE_LEFT_EDGE_MASK;
+        dMove = (dMove >> down);
+        lMove = (lMove << leftt)  & SINGLE_RIGHT_EDGE_MASK;
+
+        ptr[blackCapturedSquare] |= uMove | rMove | dMove | lMove;
+
+        uMove &= ptr[emptySquare];
+        rMove &= ptr[emptySquare];
+        dMove &= ptr[emptySquare];
+        lMove &= ptr[emptySquare];
+    }
+}
+        void computeSquareCapturedByBlackQueen(BitBoardPtr ptr)
+{
+    std::bitset<64> uMove  = ptr[blackQueen]; //u-up/r-right/d-down/l-left
+    std::bitset<64> urMove = ptr[blackQueen];
+    std::bitset<64> rMove  = ptr[blackQueen];
+    std::bitset<64> drMove = ptr[blackQueen];
+    std::bitset<64> dMove  = ptr[blackQueen];
+    std::bitset<64> dlMove = ptr[blackQueen];
+    std::bitset<64> lMove  = ptr[blackQueen];
+    std::bitset<64> ulMove = ptr[blackQueen];
+
+    while((uMove | urMove | rMove | drMove | dMove | dlMove | lMove | ulMove).any())
+    {
+        uMove  = (uMove  << up);
+        urMove = (urMove << upRight)   & SINGLE_LEFT_EDGE_MASK;
+        rMove  = (rMove  >> rightt)    & SINGLE_LEFT_EDGE_MASK;
+        drMove = (drMove >> downRight) & SINGLE_LEFT_EDGE_MASK;
+        dMove  = (dMove  >> down);
+        dlMove = (dlMove >> downLeft)  & SINGLE_RIGHT_EDGE_MASK;
+        lMove  = (lMove  << leftt)     & SINGLE_RIGHT_EDGE_MASK;
+        ulMove = (ulMove << upLeft)    & SINGLE_RIGHT_EDGE_MASK;
+
+        ptr[blackCapturedSquare] |= uMove | urMove | rMove | drMove | dMove | dlMove | lMove | ulMove;
+
+        uMove  &= ptr[emptySquare];
+        urMove &= ptr[emptySquare];
+        rMove  &= ptr[emptySquare];
+        drMove &= ptr[emptySquare];
+        dMove  &= ptr[emptySquare];
+        dlMove &= ptr[emptySquare];
+        lMove  &= ptr[emptySquare];
+        ulMove &= ptr[emptySquare];
+    }
+}
+        void computeSquareCapturedByBlackKing(BitBoardPtr ptr)
+{
+    ptr[blackCapturedSquare] |= (ptr[blackKing] << up);
+    ptr[blackCapturedSquare] |= (ptr[blackKing] << upRight) & SINGLE_LEFT_EDGE_MASK;
+    ptr[blackCapturedSquare] |= (ptr[blackKing] >> rightt) & SINGLE_LEFT_EDGE_MASK;
+    ptr[blackCapturedSquare] |= (ptr[blackKing] >> downRight) & SINGLE_LEFT_EDGE_MASK;
+    ptr[blackCapturedSquare] |= (ptr[blackKing] >> down);
+    ptr[blackCapturedSquare] |= (ptr[blackKing] >> downLeft) & SINGLE_RIGHT_EDGE_MASK;
+    ptr[blackCapturedSquare] |= (ptr[blackKing] << leftt) & SINGLE_RIGHT_EDGE_MASK;
+    ptr[blackCapturedSquare] |= (ptr[blackKing] << upLeft) & SINGLE_RIGHT_EDGE_MASK;
 }
 void writeChessboard(BitBoardPtr BBPtr)
 {
@@ -160,7 +473,7 @@ void writeChessboard(BitBoardPtr BBPtr)
         for (int j = 0; j < 8; j++)
             for (int k = 0; k < 12; k++)
             {
-                if(BBPtr[k].test(63-i*8-j) == true)
+                if(BBPtr[k].test(i*8+j) == true)
                     switch (k)
                     {
                         case whitePawn:   CBPtr[i][j] = 'P'; k = 12; break;
@@ -180,4 +493,21 @@ void writeChessboard(BitBoardPtr BBPtr)
                     CBPtr[i][j] = '.';
             }
     return CBPtr;
+}
+void writeBitSet(const std::bitset<64> &bs)
+{
+    for (int i = 63; i >= 0; i--)
+    {
+        std::cout << bs[i] << " ";
+        if(i%8 == 0)
+            std::cout << std::endl;
+    }
+}
+bool isWhiteKingChecked(BitBoardPtr ptr)
+{
+    return (ptr[whiteKing] & ptr[blackCapturedSquare]).any();
+}
+bool isBlackKingChecked(BitBoardPtr ptr)
+{
+    return (ptr[blackKing] & ptr[whiteCapturedSquare]).any();
 }
